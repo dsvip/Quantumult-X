89f19f38ffe5f4d6d943ca708f372c2cda7ecbda
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-09 14:31:39 +0800

    Update and rename Game/djdzz.js to 比赛/djdzz.js

--- 比赛/djdzz.js
+++ 比赛/djdzz.js
@@ -3,18 +3,15 @@
 脚本功能：刀剑大作战+无限钻石金币
 下载地址：https://is.gd/RSGWg0
 软件版本：1.4.0
-脚本作者：彭于晏
+脚本作者：dsvip
 更新时间：2022-9-25
-问题反馈：QQ+89996462
-QQ会员群：779392027
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py
+问题反馈：QQ+2910623
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 [rewrite_local]
 
-^https:\/\/bladecn\.mofishgames\.com\/bladesrv\/login url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/yx/djdzz.js
+^https:\/\/bladecn\.mofishgames\.com\/bladesrv\/login url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/Game/djdzz.js
 
 [mitm] 
 
