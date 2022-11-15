1370002db28f1727afcd3341e095682ec3b54633
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-15 17:56:20 +0800

    Create csxd.js

---
+++ yx/csxd.js
@@ -0,0 +1,26 @@
+/******************************
+
+脚本功能：超神小队——解锁金币
+下载地址：#微信小程序://超神小队
+软件版本：6666
+脚本作者：dsvip
+更新时间：2022-11-12
+问题反馈：QQ+2910623
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/gjyklslb\.mantianfei\.net url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/yx/csxd.js
+
+[mitm]
+
+hostname = gjyklslb.mantianfei.net
+
+****************************************/
+
+
+body = $response.body.replace(/\"num":\d+/g, '\"num" :9999999')
+
+$done({body});
