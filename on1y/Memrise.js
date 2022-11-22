3d818a4deef94597f27e8e31537293309794919a
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 16:36:06 +0800

    Update Memrise.js

--- on1y/Memrise.js
+++ on1y/Memrise.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.memrise\.com\/v1\.19 url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Memrise.js
+^https:\/\/api\.memrise\.com\/v1\.19 url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Memrise.js
 
 [mitm] 
 
