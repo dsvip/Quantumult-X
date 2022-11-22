c0ed4c33f80780d9a20b0ea2821ee1aefa55e0fd
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 17:43:20 +0800

    Update Seven.js

--- on1y/Seven.js
+++ on1y/Seven.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://api.sevenworkouts.com/api/v2/verify-purchases url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Seven.js
+https://api.sevenworkouts.com/api/v2/verify-purchases url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Seven.js
 
 [mitm] 
 
