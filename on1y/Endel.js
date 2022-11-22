a16e8d66a4bf4c117962d390ace246c17f342548
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 16:24:56 +0800

    Update Endel.js

--- on1y/Endel.js
+++ on1y/Endel.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api-production\.endel\.io\/v4\/call url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Endel.js
+^https:\/\/api-production\.endel\.io\/v4\/call url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Endel.js
 
 [mitm] 
 
