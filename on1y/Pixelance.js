14edd2033d77ab224211f1ea8295d45f2b1c260f
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 17:37:14 +0800

    Update Pixelance.js

--- on1y/Pixelance.js
+++ on1y/Pixelance.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/iap\.etm\.tech\/receipts url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Pixelance.js
+^https:\/\/iap\.etm\.tech\/receipts url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Pixelance.js
 
 [mitm] 
 
