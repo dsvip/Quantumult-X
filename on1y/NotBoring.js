0b78cead84e4d46b6dc552b94f508dfd3cccbc2a
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 17:17:09 +0800

    Update NotBoring.js

--- on1y/NotBoring.js
+++ on1y/NotBoring.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/NotBoring.js
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/NotBoring.js
 
 [mitm] 
 
