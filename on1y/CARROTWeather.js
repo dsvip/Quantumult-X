489664b0a7a5159f9f3b30e95e6477cdcb12905a
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 15:38:41 +0800

    Update CARROTWeather.js

--- on1y/CARROTWeather.js
+++ on1y/CARROTWeather.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/carrotweather\.herokuapp\.com\/parse\/functions\/findSubscriptionsForUserId url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/CARROTWeather.js
+^https:\/\/carrotweather\.herokuapp\.com\/parse\/functions\/findSubscriptionsForUserId url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/CARROTWeather.js
 
 [mitm] 
 
