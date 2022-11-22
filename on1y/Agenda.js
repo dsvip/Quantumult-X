f30f66a8fce8580abbf38910935ccc672eb46c40
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 15:36:33 +0800

    Update Agenda.js

--- on1y/Agenda.js
+++ on1y/Agenda.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/accounts\.agenda\.com\/users\/.*\/license url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Agenda.js
+^https:\/\/accounts\.agenda\.com\/users\/.*\/license url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Agenda.js
 
 [mitm] 
 
