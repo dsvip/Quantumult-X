0526505eeed9ca7e49bbfabfd3373610316b879f
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 15:38:19 +0800

    Update Busuu.js

--- on1y/Busuu.js
+++ on1y/Busuu.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/(.*.*.)\/users\/me url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Busuu.js
+^https?:\/\/(.*.*.)\/users\/me url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Busuu.js
 
 hostname = *.boshu.*,*.bosuu.com,*.busuu.com
 
