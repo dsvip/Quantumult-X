a724213e38a4c4c78fd05cd315736dd7c04e4d8b
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 16:25:16 +0800

    Update FitnessView.js

--- on1y/FitnessView.js
+++ on1y/FitnessView.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/FitnessView.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/FitnessView.js
 
 [mitm] 
 
