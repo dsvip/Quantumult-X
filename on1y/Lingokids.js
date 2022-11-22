3d20dc1e332b1e23c298b5dfb995e1b231a989ea
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 16:35:25 +0800

    Update Lingokids.js

--- on1y/Lingokids.js
+++ on1y/Lingokids.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.lingokids\.com\/v1\/renovate_session url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Lingokids.js
+^https:\/\/api\.lingokids\.com\/v1\/renovate_session url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Lingokids.js
 
 [mitm] 
 
