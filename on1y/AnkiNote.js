c27d80e04c4785ba662ba0ebae7e3215d8f124ad
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 15:37:03 +0800

    Update AnkiNote.js

--- on1y/AnkiNote.js
+++ on1y/AnkiNote.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/AnkiNote.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/AnkiNote.js
 
 [mitm] 
 
