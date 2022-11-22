fb3bde5cd06dea45db7b024b6eec6a799e0d81cc
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 09:27:31 +0800

    Update AAnybox.js

--- on1y/AAnybox.js
+++ on1y/AAnybox.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(.*?)*$) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/AAnybox.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(.*?)*$) url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/AAnybox.js
 
 [mitm] 
 
