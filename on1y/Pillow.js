e824b83963f079fe1c9da06dc126ad6c84998e4e
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 17:36:47 +0800

    Update Pillow.js

--- on1y/Pillow.js
+++ on1y/Pillow.js
@@ -14,7 +14,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Pillow.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Pillow.js
 
 [mitm] 
 
