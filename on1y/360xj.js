03a952c2c6ddbb297936e9cf3dbb57bbc4ba5b9a
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 09:25:01 +0800

    Update 360xj.js

--- on1y/360xj.js
+++ on1y/360xj.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/bmall\.camera360\.com\/api\/iap\/check-receipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/360xj.js
+^https:\/\/bmall\.camera360\.com\/api\/iap\/check-receipt url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/360xj.js
 
 [mitm] 
 
