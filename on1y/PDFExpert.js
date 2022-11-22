fd3463ee70992238a8e44c8b60f3b44966807777
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 17:22:43 +0800

    Update PDFExpert.js

--- on1y/PDFExpert.js
+++ on1y/PDFExpert.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/pdfexpert6\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/PDFExpert.js
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/pdfexpert6\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/PDFExpert.js
 
 [mitm] 
 
