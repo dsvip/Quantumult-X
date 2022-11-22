810b19223617751b33a501179d7c9508e0dbaa6c
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-22 16:14:30 +0800

    Update Drafts.js

--- on1y/Drafts.js
+++ on1y/Drafts.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/backend\.getdrafts\.com\/api\/v1\/verification\/verify_receipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Drafts.js
+^https:\/\/backend\.getdrafts\.com\/api\/v1\/verification\/verify_receipt url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Drafts.js
 
 [mitm] 
 
