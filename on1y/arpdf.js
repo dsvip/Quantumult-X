b62112b5489ba1696166da955adc1077d6fa5cd8
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:08:30 +0800

    Update arpdf.js

--- on1y/arpdf.js
+++ on1y/arpdf.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/gw\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/arpdf.js
+^https:\/\/gw\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/arpdf.js
 
 [mitm] 
 
