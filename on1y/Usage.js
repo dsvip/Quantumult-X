c0d0649aec2a363fa79b4a068121d3dfb0d8d7bd
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:06:04 +0800

    Update Usage.js

--- on1y/Usage.js
+++ on1y/Usage.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/v1\/subscribers.+ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Usage.js
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers.+ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Usage.js
 
 [mitm] 
 hostname = api.revenuecat.com
