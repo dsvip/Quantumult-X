f80028a75b399bec6b1557c1b4050fc57cd86444
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:06:33 +0800

    Update Varlens.js

--- on1y/Varlens.js
+++ on1y/Varlens.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Varlens.js
+^https:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Varlens.js
 
 [mitm] 
 
