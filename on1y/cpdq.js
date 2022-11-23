780ef702ef45f954d0c9990378c0ae0ed9c69738
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:13:47 +0800

    Update cpdq.js

--- on1y/cpdq.js
+++ on1y/cpdq.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/apiios_dq\.xiangha\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/cpdq.js
+^https:\/\/apiios_dq\.xiangha\.com url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/cpdq.js
 
 [mitm] 
 
