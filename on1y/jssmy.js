cedb74adcb6937273efda1e95e88471a141a7e11
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:23:48 +0800

    Update jssmy.js

--- on1y/jssmy.js
+++ on1y/jssmy.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/scanner\.jianse\.tv\/api\/users\/loginUser url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/jssmy.js
+^https:\/\/scanner\.jianse\.tv\/api\/users\/loginUser url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/jssmy.js
 
 [mitm] 
 
