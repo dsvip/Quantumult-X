a6cd07637cfd9dccd63927b6d04bf35f9c5553f5
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:14:22 +0800

    Update darkroom.js

--- on1y/darkroom.js
+++ on1y/darkroom.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/darkroom.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/darkroom.js
 
 [mitm] 
 
