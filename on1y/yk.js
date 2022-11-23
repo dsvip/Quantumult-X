ec8c259cd79d36a43e6c1c09e740e33413a88230
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:48:17 +0800

    Update yk.js

--- on1y/yk.js
+++ on1y/yk.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/gw\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yk.js
+^https:\/\/gw\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/yk.js
 
 [mitm] 
 
