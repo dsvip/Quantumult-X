bf7d8f5c1a9bafbcf48a6c7c4efdced41a134d06
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:13:33 +0800

    Update ceo.js

--- on1y/ceo.js
+++ on1y/ceo.js
@@ -17,7 +17,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/user\.xiaozaoapp\.com\/app\/ceouser\/loginAuto url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ceo.js
+^https:\/\/user\.xiaozaoapp\.com\/app\/ceouser\/loginAuto url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/ceo.js
 
 [mitm] 
 hostname = user.xiaozaoapp.com
