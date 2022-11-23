91a1b74ebfaa71474f9e67b05cc78364e12679e2
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:26:52 +0800

    Update lovePdf.js

--- on1y/lovePdf.js
+++ on1y/lovePdf.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://service.ilovepdf.com/v1/user url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/lovePdf.js
+https://service.ilovepdf.com/v1/user url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/lovePdf.js
 
 
 [mitm] 
