2785a637b0745b9933b47c2f00d228fec20aaf35
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:34:22 +0800

    Update ptyxj.js

--- on1y/ptyxj.js
+++ on1y/ptyxj.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/puzzle\.xcxsc\.net\/v5\/user\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ptyxj.js
+^https:\/\/puzzle\.xcxsc\.net\/v5\/user\/info url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/ptyxj.js
 
 [mitm] 
 
