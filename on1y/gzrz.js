47c37d272568d3604bcbfdddbb0728aec46fcaf4
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:19:54 +0800

    Update gzrz.js

--- on1y/gzrz.js
+++ on1y/gzrz.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/diary-id\.sumi\.io\/api\/profile url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/gzrz.js
+^https:\/\/diary-id\.sumi\.io\/api\/profile url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/gzrz.js
 
 [mitm] 
 
