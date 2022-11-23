248815ba27174e7bf3e06ca12874dff9bfef182d
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:18:13 +0800

    Update fkxxx.js

--- on1y/fkxxx.js
+++ on1y/fkxxx.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py
 *******************************
 [rewrite_local]
 
-^https:\/\/wxsdk-data\.d3games\.com\/game\/merge\/get url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fkxxx.js
+^https:\/\/wxsdk-data\.d3games\.com\/game\/merge\/get url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/fkxxx.js
 
 [mitm] 
 
