63610aefd5f63d9610d52c3598477f1d7a62951f
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:40:31 +0800

    Update ubestkid.js

--- on1y/ubestkid.js
+++ on1y/ubestkid.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/aicblhapp.ubestkid.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ubestkid.js
+^http[s]?:\/\/aicblhapp.ubestkid.com url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/ubestkid.js
 
 [mitm] 
 
