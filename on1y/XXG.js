81b298e8989dd354bf89eb4848344a53e30cece1
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:07:45 +0800

    Update XXG.js

--- on1y/XXG.js
+++ on1y/XXG.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/xianbeikeji\.com\/daily\/app\/user\/query url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/XXG.js
+^https:\/\/xianbeikeji\.com\/daily\/app\/user\/query url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/XXG.js
 
 [mitm] 
 
