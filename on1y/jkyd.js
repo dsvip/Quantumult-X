112ff8911bf44f1ad2f241b47976471b1ca1a58d
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:23:32 +0800

    Update jkyd.js

--- on1y/jkyd.js
+++ on1y/jkyd.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/ec\.myjkyd\.com\/ftec url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/jkyd.js
+http:\/\/ec\.myjkyd\.com\/ftec url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/jkyd.js
 
 [mitm] 
 
