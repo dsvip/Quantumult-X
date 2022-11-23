98ff4a7d05c2867d80d6f2a64e551780fd9ae1dd
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:19:41 +0800

    Update gql.js

--- on1y/gql.js
+++ on1y/gql.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/expire\.vanemu\.com\/1\.1\/functions\/getUserInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/gql.js
+^https:\/\/expire\.vanemu\.com\/1\.1\/functions\/getUserInfo url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/gql.js
 
 [mitm] 
 
