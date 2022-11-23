a0832b39a55f2a18aaccfd3dc627e2529ee47c36
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:38:10 +0800

    Update smqnw.js

--- on1y/smqnw.js
+++ on1y/smqnw.js
@@ -14,7 +14,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/smqnw.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/smqnw.js
 
 [mitm]
 
