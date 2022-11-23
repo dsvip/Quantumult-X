71758d56377bfbeb85b9af6aa27ebaa24daa2f2e
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:16:56 +0800

    Update egdd.js

--- on1y/egdd.js
+++ on1y/egdd.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/gateway\.ergedd\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/egdd.js
+http:\/\/gateway\.ergedd\.com url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/egdd.js
 
 [mitm] 
 
