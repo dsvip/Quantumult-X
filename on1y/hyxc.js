c00ac86221c20e615a8fdf98d21e5b36d9bb6805
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:21:23 +0800

    Update hyxc.js

--- on1y/hyxc.js
+++ on1y/hyxc.js
@@ -13,7 +13,7 @@
 
 [rewrite_local]
 
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/hyxc.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/hyxc.js
 
 [mitm] 
 
