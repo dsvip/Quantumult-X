64c9e025c9c74f030f5b0f0b1100244762a5aba4
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:34:53 +0800

    Update qmzj.js

--- on1y/qmzj.js
+++ on1y/qmzj.js
@@ -17,7 +17,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/qmzj.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/qmzj.js
 
 [mitm] 
 
