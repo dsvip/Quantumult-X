0c0d5c59091f882799d4974a467d3a24e13ca0e5
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:28:50 +0800

    Update mgtv.js

--- on1y/mgtv.js
+++ on1y/mgtv.js
@@ -15,7 +15,7 @@ TG群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/fcapi\.91por\.vip\/api\/video\/user\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/mgtv.js
+^https:\/\/fcapi\.91por\.vip\/api\/video\/user\/info url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/mgtv.js
 
 [mitm] 
 hostname = fcapi.91por.vip
