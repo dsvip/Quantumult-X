fc2d9939497595e55147d95040e32ff90fc7afe6
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:42:34 +0800

    Update wfzy.js

--- on1y/wfzy.js
+++ on1y/wfzy.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-http:\/\/iosv2\.cjapi\.wufan88\.com\/user\/.+ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wfzy.js
+http:\/\/iosv2\.cjapi\.wufan88\.com\/user\/.+ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/wfzy.js
 
 [mitm] 
 
