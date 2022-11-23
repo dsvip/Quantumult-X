406b491647f94859acd12855fc483b5b220ced47
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:44:32 +0800

    Update wsgj.js

--- on1y/wsgj.js
+++ on1y/wsgj.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api-v3\.speedtest\.cn\/user\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wsgj.js
+^https:\/\/api-v3\.speedtest\.cn\/user\/info url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/wsgj.js
 
 [mitm] 
 
