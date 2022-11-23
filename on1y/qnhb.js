8a6364737e079bc73780e5d51c8f29dcb5afae6c
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:35:18 +0800

    Update qnhb.js

--- on1y/qnhb.js
+++ on1y/qnhb.js
@@ -17,7 +17,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/api\.qingning6\.com\/api\/user\/getUserInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/qnhb.js
+^https:\/\/api\.qingning6\.com\/api\/user\/getUserInfo url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/qnhb.js
 
 [mitm] 
 
