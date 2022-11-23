8f68ba00a7e7b787f03e2d57f88a00debfffeea8
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:16:42 +0800

    Update dzqjzz.js

--- on1y/dzqjzz.js
+++ on1y/dzqjzz.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 *******************************
 [rewrite_local]
 
-^https:\/\/cn\.invit\.vip\/users\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/dzqjzz.js
+^https:\/\/cn\.invit\.vip\/users\/info url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/dzqjzz.js
 
 [mitm] 
 
