5b7720216ffbab4a5b510591098ee2a68d61fcf1
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:17:43 +0800

    Update fileball.js

--- on1y/fileball.js
+++ on1y/fileball.js
@@ -17,7 +17,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fileball.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/fileball.js
 
 ^https:\/\/app-measurement\.com\/config\/app\/1?(.*?)*$ url reject
 
