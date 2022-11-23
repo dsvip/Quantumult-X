1a48a6e75575cfa03ea29e4b5fe2942aacc87633
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:12:23 +0800

    Update budingsuoping.js

--- on1y/budingsuoping.js
+++ on1y/budingsuoping.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https?:\/\/screen-lock\.51wnl-cq\.com\/userApi\/saveUser url script-echo-response https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/budingsuoping.js
+https?:\/\/screen-lock\.51wnl-cq\.com\/userApi\/saveUser url script-echo-response https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/budingsuoping.js
 
 [mitm] 
 
