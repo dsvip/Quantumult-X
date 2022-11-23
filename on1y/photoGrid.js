6c054c5ba378e040562e5cee4361a1b2c4f98887
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:33:36 +0800

    Update photoGrid.js

--- on1y/photoGrid.js
+++ on1y/photoGrid.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/pgapi\.ksmobile\.com\/v1\/ios\/auth url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/photoGrid.js
+^https:\/\/pgapi\.ksmobile\.com\/v1\/ios\/auth url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/photoGrid.js
 
 [mitm] 
 
