00e43a81a8c509657d125e3c12fee8bfc19db4a0
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:40:57 +0800

    Update upp.js

--- on1y/upp.js
+++ on1y/upp.js
@@ -13,7 +13,7 @@
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/upp.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/upp.js
 
 [mitm] 
 hostname = buy.itunes.apple.com
