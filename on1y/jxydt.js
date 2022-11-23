d0f7869aff861e2d5706b87c0821bfee2edc4dad
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:24:02 +0800

    Update jxydt.js

--- on1y/jxydt.js
+++ on1y/jxydt.js
@@ -14,7 +14,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/vipapi\.jxedt\.com\/store\/vip\/check url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/jxydt.js
+^https:\/\/vipapi\.jxedt\.com\/store\/vip\/check url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/jxydt.js
 
 [mitm] 
 hostname = vipapi.jxedt.com
