1539e26bd3dd44d102466f12ca10a0c5248538f8
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:08:01 +0800

    Update abushuiqiangushi.js

--- on1y/abushuiqiangushi.js
+++ on1y/abushuiqiangushi.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/tutugushiapi\.tutuerge\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/abushuiqiangushi.js
+http:\/\/tutugushiapi\.tutuerge\.com url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/abushuiqiangushi.js
 
 [mitm] 
 
