292c9d96ea7d5de7e88648d4696466f84ea528d4
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:40:45 +0800

    Update umu168.js

--- on1y/umu168.js
+++ on1y/umu168.js
@@ -11,7 +11,7 @@ TG频道群：https://t.me/py996
 *******************************
 [rewrite_local]
 
-^http[s]?:\/\/api3.umu168.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/umu168.js
+^http[s]?:\/\/api3.umu168.com url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/umu168.js
 
 [mitm] 
 
