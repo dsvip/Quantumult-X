8a3f5b76e9f27fa6ef64fb4b7f25a8e28eed3bb4
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:40:16 +0800

    Update txwxds.js

--- on1y/txwxds.js
+++ on1y/txwxds.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/txwxds.js
+^https:\/\/i\.weread\.qq\.com\/pay\/memberCardSummary url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/txwxds.js
 
 [mitm] 
 
