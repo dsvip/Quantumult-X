2e2bd233e8ea148a862261834a93f9d774038dfe
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:27:23 +0800

    Update ly.js

--- on1y/ly.js
+++ on1y/ly.js
@@ -13,7 +13,7 @@
 
 [rewrite_local]
 
-^https:\/\/partner-ai-api\.quthing\.com\/ai\/vip\/state url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ly.js
+^https:\/\/partner-ai-api\.quthing\.com\/ai\/vip\/state url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/ly.js
 
 [mitm] 
 hostname = partner-ai-api.quthing.com
