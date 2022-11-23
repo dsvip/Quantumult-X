50656eb1426246dc189562ca3acbf7810b9055d9
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:35:32 +0800

    Update qwtodo.js

--- on1y/qwtodo.js
+++ on1y/qwtodo.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/frogtodo\.shuge888\.com:33333\/account\/refreshState url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/qwtodo.js
+^https:\/\/frogtodo\.shuge888\.com:33333\/account\/refreshState url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/qwtodo.js
 
 [mitm] 
 
