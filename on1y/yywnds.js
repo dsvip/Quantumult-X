9cae38a65567e014efcd8cda271644e77a669d91
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:49:45 +0800

    Update yywnds.js

--- on1y/yywnds.js
+++ on1y/yywnds.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yywnds.js
+^https:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/yywnds.js
 
 [mitm] 
 
