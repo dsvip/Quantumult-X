c19514f545f3b7fa59864fa2afa1a27564a3c89c
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:33:07 +0800

    Update pandavip.js

--- on1y/pandavip.js
+++ on1y/pandavip.js
@@ -14,7 +14,7 @@ TG频道群：https://t.me/py
 *******************************
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/pandavip.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/pandavip.js
 
 [mitm]
 
