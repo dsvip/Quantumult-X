97f77a4aa0916d82a0c01b69112347472dc58e14
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:14:51 +0800

    Update ddqd.js

--- on1y/ddqd.js
+++ on1y/ddqd.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ddqd.js
+^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/ddqd.js
 
 [mitm] 
 
