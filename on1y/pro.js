0f6a97327ef2f4c92d708335e000b743e8eb2c2e
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-11-23 14:33:52 +0800

    Update pro.js

--- on1y/pro.js
+++ on1y/pro.js
@@ -14,7 +14,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/pro.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/pro.js
 
 [mitm] 
 hostname = buy.itunes.apple.com
