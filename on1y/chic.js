8195ae213feb1e86eb54ad34237f72a470cfffdb
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-12-03 15:44:46 +0800

    Update and rename Chic.js to chic.js

--- on1y/chic.js
+++ on1y/chic.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info\.json url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Chic.js
+^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info\.json url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/chic.js
 
 [mitm] 
 
