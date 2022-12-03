7bdbd7dcd3d25c510a24087b5463f012d0204764
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2022-12-03 15:45:07 +0800

    Update and rename Busuu.js to busuu.js

--- on1y/busuu.js
+++ on1y/busuu.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/(.*.*.)\/users\/me url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/Busuu.js
+^https?:\/\/(.*.*.)\/users\/me url script-response-body https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/busuu.js
 
 hostname = *.boshu.*,*.bosuu.com,*.busuu.com
 
