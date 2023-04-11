64e9ebd4f4e20a47553a571754c50ad0d74257e6
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2023-04-11 08:26:41 +0800

    Add files via upload

---
+++ chxm1023/Task/TieBa/TieBa_cookie.js
@@ -0,0 +1,6 @@
+# 百度贴吧每日自动签到获取Cookie
+^https?:\/\/(c\.tieba\.baidu\.com|180\.97\.\d+\.\d+)\/c\/s\/login url script-request-header https://raw.githubusercontent.com/chxm1023/Task/main/TieBa/TieBa_signin.js
+^https?:\/\/c\.tieba\.baidu\.com\/c\/s\/channelIconConfig url script-request-header https://raw.githubusercontent.com/chxm1023/Task/main/TieBa/TieBa_signin.js
+^https?:\/\/tiebac\.baidu\.com\/c\/u\/follow\/getFoldedMessageUserInfo url script-request-header https://raw.githubusercontent.com/chxm1023/Task/main/TieBa/TieBa_signin.js
+
+hostname= c.tieba.baidu.com
