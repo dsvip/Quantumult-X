6b8dfce5bbb1b13bbf617d4c9609f682718efc7c
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2023-04-11 08:27:52 +0800

    Add files via upload

---
+++ chxm1023/Task/JD/jd_cookie.js
@@ -0,0 +1,4 @@
+# 京东Cookie 登录网址：https://bean.m.jd.com/bean/signIndex.action
+^https:\/\/(api\.m|me-api)\.jd\.com\/(client\.action\?functionId=signBean|user_new\/info\/GetJDUserInfoUnion\?) url script-request-header https://raw.githubusercontent.com/chxm1023/Task/main/JD/JD_DailyBonus.js
+
+hostname = ms.jr.jd.com, me-api.jd.com, api.m.jd.com
