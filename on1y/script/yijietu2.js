149580c9a27938801fe8a9d32913a3fbdaf5e0be
Nikola Ds <117161448+dsvip@users.noreply.github.com>
2023-05-09 00:27:42 +0800

    Update yijietu2.js

--- on1y/script/yijietu2.js
+++ on1y/script/yijietu2.js
@@ -8,12 +8,13 @@ TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
-[rewrite_local] https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/script/yijietu2.js
-^http[s]?:\/\/jietu\d.com\/apiv2\/user url script-response-body com.js
+[rewrite_local]
+
+^http[s]?:\/\/jietu\d.com\/apiv2\/user url script-response-body  https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/script/yijietu2.js
 //或者
-^http[s]?:\/\/jietu*.com\/apiv2\/user url script-response-body com.js
+^http[s]?:\/\/jietu*.com\/apiv2\/user url script-response-body  https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/script/yijietu2.js
 //或者
-^http[s]?:\/\/(jietu6|jietu8).com\/apiv2\/user url script-response-body com.js
+^http[s]?:\/\/(jietu6|jietu8).com\/apiv2\/user url script-response-body  https://raw.githubusercontent.com/dsvip/Quantumult-X/main/on1y/script/yijietu2.js
 [mitm] 
 hostname = jietu6.com|jietu8.com
 
